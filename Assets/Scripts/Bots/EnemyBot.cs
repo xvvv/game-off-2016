ff45a5f793d58523bde6327525e94663cdd556df
xvvv <xvvv003@gmail.com>
2016-11-10 13:41:04 +0100

    Revert "Revert "The unity project""
    This reverts commit 78554756ca8f569b293c483d7e21b9784c52b97a.

---
+++ Assets/Scripts/Bots/EnemyBot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyBot : MonoBehaviour {
+
+    [SerializeField]
+    private BotType botType = BotType.NORMAL;
+
+    protected virtual void Start() {
+        /*Empty*/
+    }
+
+    protected virtual void Update() {
+        /*Empty*/
+    }
+
+    public BotType getBotType() {
+        return botType;
+    }
+
+    public void setBotType(BotType botType) {
+        this.botType = botType;
+    }
+
+}
---
+++ Assets/Scripts/Bots/PlayerBot.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerBot : MonoBehaviour {
+
+    private Vector3 moveDirection;
+
+    //TODO change accessors
+    [SerializeField]
+    private BotType botType = BotType.NORMAL;
+
+    public float moveSpeed = 5;
+    public float rotationSpeed = 2.5F;
+    public Transform modelTransform;
+
+    private BoxCollider bc;
+
+	void Start () {
+        //Retrieve Components
+        bc = GetComponent<BoxCollider>();
+
+    }
+	
+	void Update () {
+        handleInput();
+    }
+
+    private void handleInput() {/*
+        if (Input.GetKey(KeyCode.LeftArrow))    //Roteer model naar links
+            modelTransform.Rotate(-transform.up * rotationSpeed);
+
+        if (Input.GetKey(KeyCode.RightArrow))   //Roteer model naar rechts
+            modelTransform.Rotate(transform.up * rotationSpeed);*/
+
+        modelTransform.Rotate(transform.up * rotationSpeed * Input.GetAxis("Horizontal"));
+
+        if (Input.GetKey(KeyCode.UpArrow))      //Beweeg naar voren
+            RPhysics.Move(transform, modelTransform.forward, moveSpeed);
+
+        if (Input.GetKey(KeyCode.DownArrow))    //Beweeg naar achteren
+            RPhysics.Move(transform, -modelTransform.forward, moveSpeed);
+
+        if (InputManager.Jump)
+            grab();
+    }
+
+    private void grab() {
+        print("Attempting to grab...");
+
+        RaycastHit hit = RPhysics.BoxCast(transform, modelTransform.forward);
+        if (hit.transform.GetComponent<EnemyBot>()) {
+            swapTypes(hit.collider.GetComponent<EnemyBot>());
+            print("Grabbed an enemy! " + hit.transform.name);
+        }
+
+        print(hit.transform.name);
+
+    }
+
+    private void swapTypes(EnemyBot enemy) {
+        BotType temp = botType;
+        botType = enemy.getBotType();
+        enemy.setBotType(temp);
+    }
+
+}
